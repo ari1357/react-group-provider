10190a4ca8ee363e19e0da79cd13624a17d3f822
ari1357 <arimura1379@gmail.com>
2023-09-26 20:49:45 +0900

    update: setting path alias

--- packages/react-group-provider/vite.config.ts
+++ packages/react-group-provider/vite.config.ts
@@ -2,11 +2,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import type { InlineConfig } from 'vitest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
